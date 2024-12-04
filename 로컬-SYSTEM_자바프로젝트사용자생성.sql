a3891de277258d95111414ea18487914f8550431
jangpyo <wkdvy122851@gmail.com>
2024-12-04 19:26:48 +0900

    [CREATE] JSP web DB 로컬 계정 생성

--- 로컬-SYSTEM_자바프로젝트사용자생성.sql
+++ 로컬-SYSTEM_자바프로젝트사용자생성.sql
@@ -29,3 +29,11 @@ CREATE USER javauser IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번�
     DEFAULT TABLESPACE javadata -- 데이터 저장소
     TEMPORARY TABLESPACE TEMP; -- 임시 저장 장소
 GRANT connect, resource, dba TO javauser; -- 권한 부여
+
+-- 웹 프로젝트 사용자 계정 생성
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+DROP USER webuser CASCADE; -- 기존 사용자 삭제
+CREATE USER webuser IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS -- 데이터 저장소
+    TEMPORARY TABLESPACE TEMP; -- 임시 저장 장소
+GRANT connect, resource, dba TO webuser; -- 권한 부여
